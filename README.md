1bb2bad0a4fbbbcb8c12f349a56a0669a4b2f2d2
Ahmed Elaidy <alaidy.y123@gmail.com>
2022-07-31 14:24:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Main Text 
+<hr>
+E-commerce App readme file
+<hr>
+## The languages that i used:
+<hr>
+- REACT <br>
+- JAVASCRIPT <br>
+- HTML <br>
+- CSS <br>
+<hr>
+## The Functionalities:
+<hr>
+- The E-commerce App project is a react project that has a lot of functions and I fixed a lot of problems that I faced while developing it. <br>
+- One of the problems that I faced was in payment stage that when the payment is done the cart still has the items so i made a function the empty the cart after payment. <br>
+- another problem i faced in the last payment step was that it takes a lot of time and nothing happens so i put a loader there. <br>
+<hr>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import Logo from "../../assets/commerce.png";
+import useStyles from "./styles";
+import { Link, useLocation } from "react-router-dom";
+
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  return (
+    <React.Fragment>
+      <AppBar position="fixed" className={classes.navbar} color="inherit">
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={Logo}
+              alt="Elady E-Store"
+              height="25px"
+              className={classes.image}
+            />
+            Elaidy Store
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === "/" && (
+            <div className={classes.button}>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={totalItems} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+export default Navbar;
